85bf22a10bc847032956a68cfb698836ee696211
This is Rude <dbauman1@swarthmore.edu>
2017-12-30 18:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     console.log("Sorry, we don't have a credit card on file for you.")
     return
-  } else{
+  } else {
     var moola = total()
     console.log(`Your total cost is ${moola}, which will be charged to the card ${cardNumber}.`)
     var cartarray = Object.keys(cart)
