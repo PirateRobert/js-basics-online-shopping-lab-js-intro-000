3c9de8bf767ac02e44e724a5cd33f116cdc92a97
This is Rude <dbauman1@swarthmore.edu>
2017-12-30 16:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function total() {
 function removeFromCart(item) {
   if (cart.hasOwnProperty(item)){
     delete cart[item]
+    return cart
   } else{
     console.log("That item is not in your cart.")
     return cart
