8dbfe065207d35f93d3cf17ad9bb20acad089ffc
This is Rude <dbauman1@swarthmore.edu>
2017-12-30 16:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   var string = `In your cart, you have`
   for (let i = 0;i<cart.length;i++){
     if (i === 0){
-      string = string + ` ${} at ${}`
+      string = string + ` ${cartarray[i]} at ${}`
     }else if (i < cart.length-1){
 
     }
