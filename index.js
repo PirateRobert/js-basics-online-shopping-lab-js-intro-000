d6d3b3c3e0bc7ad7e1ea1d3b3bdeee47397be4d3
This is Rude <dbauman1@swarthmore.edu>
2017-12-30 18:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var newbie = {[item]: Math.floor(Math.random() * 100)}
- cart.push(newbie)
+ getCart().push(newbie)
  console.log(`${item} has been added to your cart.`)
  return cart
 }
